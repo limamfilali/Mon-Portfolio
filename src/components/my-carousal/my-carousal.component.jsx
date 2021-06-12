aaa08522f6cb4d0f8d8d08b26f002e4fb7f5add0
limamfilali <47561373+limamfilali@users.noreply.github.com>
2021-06-12 15:45:00 +0100

    deploy

---
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Slide1 from "../../assets/img/carousal/slide1.webp";
+import Slide2 from "../../assets/img/carousal/slide2.webp";
+import Slide3 from "../../assets/img/carousal/slide3.webp";
+import Slide4 from "../../assets/img/carousal/raw.jpeg";
+import ScrollDown from "../scroll-down/scroll-down.component";
+import "./my-carousal.styles.css";
+
+const MyCarousal = () => {
+  return (
+    <div id="home">
+      <Carousel fade controls={false} indicators interval={2500} pauseOnHover={false}>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide2} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide4} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide1} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+      <ScrollDown />
+    </div>
+  );
+};
+
+export default MyCarousal;
